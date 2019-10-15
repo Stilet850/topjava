5d754230599da124304ecefed6ca771b65e91ddd
Slava Rakhmvi <stilet850@mail.ru>
2019-10-15 18:37:07 +0300

    2 1 HW1

--- src/main/java/ru/javawebinar/topjava/web/MealServlet.java
+++ src/main/java/ru/javawebinar/topjava/web/MealServlet.java
@@ -3,5 +3,6 @@ package ru.javawebinar.topjava.web;
 import javax.servlet.http.HttpServlet;
 
 public class MealServlet extends HttpServlet {
+    private static final Logger log = LoggerFactory.getLogger(MealServlet.class);
 
 }
