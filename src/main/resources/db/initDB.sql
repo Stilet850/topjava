ba43eb5e0759784f41ab78569e53410ccec03ebf
Slava Rakhmvi <stilet850@mail.ru>
2019-10-23 12:06:23 +0300

    HW03. Add meals to DB.

--- src/main/resources/db/initDB.sql
+++ src/main/resources/db/initDB.sql
@@ -1,10 +1,11 @@
+DROP TABLE IF EXISTS meals;
 DROP TABLE IF EXISTS user_roles;
 DROP TABLE IF EXISTS users;
 DROP SEQUENCE IF EXISTS global_seq;
 
 CREATE SEQUENCE global_seq START WITH 100000;
 
-CREATE TABLE topjava.topjava.users
+CREATE TABLE users
 (
   id               INTEGER PRIMARY KEY DEFAULT nextval('global_seq'),
   name             VARCHAR                 NOT NULL,
@@ -21,5 +22,15 @@ CREATE TABLE user_roles
   user_id INTEGER NOT NULL,
   role    VARCHAR,
   CONSTRAINT user_roles_idx UNIQUE (user_id, role),
-  FOREIGN KEY (user_id) REFERENCES topjava.topjava.users (id) ON DELETE CASCADE
+  FOREIGN KEY (user_id) REFERENCES users (id) ON DELETE CASCADE
+);
+
+CREATE TABLE meals
+(
+    id          INTEGER PRIMARY KEY DEFAULT nextval('global_seq'),
+    user_id     INTEGER   NOT NULL,
+    description VARCHAR   NOT NULL,
+    date_time   TIMESTAMP NOT NULL,
+    calories    INTEGER   NOT NULL,
+    FOREIGN KEY (user_id) REFERENCES users (id) ON DELETE CASCADE
 );
