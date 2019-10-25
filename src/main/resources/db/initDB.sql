751ba286fbacf50586633f2761840f37fa60b33c
Slava Rakhmvi <stilet850@mail.ru>
2019-10-25 15:36:01 +0300

    3 fixes

--- src/main/resources/db/initDB.sql
+++ src/main/resources/db/initDB.sql
@@ -4,7 +4,7 @@ DROP SEQUENCE IF EXISTS global_seq;
 
 CREATE SEQUENCE global_seq START WITH 100000;
 
-CREATE TABLE topjava.topjava.users
+CREATE TABLE users
 (
   id               INTEGER PRIMARY KEY DEFAULT nextval('global_seq'),
   name             VARCHAR                 NOT NULL,
@@ -14,12 +14,12 @@ CREATE TABLE topjava.topjava.users
   enabled          BOOL DEFAULT TRUE       NOT NULL,
   calories_per_day INTEGER DEFAULT 2000    NOT NULL
 );
-CREATE UNIQUE INDEX users_unique_email_idx ON topjava.topjava.users (email);
+CREATE UNIQUE INDEX users_unique_email_idx ON users (email);
 
 CREATE TABLE user_roles
 (
   user_id INTEGER NOT NULL,
   role    VARCHAR,
   CONSTRAINT user_roles_idx UNIQUE (user_id, role),
-  FOREIGN KEY (user_id) REFERENCES topjava.topjava.users (id) ON DELETE CASCADE
+  FOREIGN KEY (user_id) REFERENCES users (id) ON DELETE CASCADE
 );
